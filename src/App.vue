302811d14e3a16d396bb6c1f571e0f6ae3f77ec4
Alitindrawan24 <alitindrawan71@gmail.com>
2021-08-17 00:53:56 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <form id="form" @submit.prevent="addNewTodo">
+            <input type="text" name="newTodo" v-model="newTodo">
+            <button type="submit">Submit</button>
+        </form>
+        <button @click="markAsDoneAll">Mark as done all</button>
+        <button @click="removeAll">Remove all</button>
+        <button @click="removeAllDone">Remove all done todo</button>
+        <ul>
+            <li v-for="(todo, index) in todos" :key="todo.id">
+                <h4 :class="{ done: todo.done }" @click="toggleTodo(todo)">{{ todo.name }}</h4>
+                <button @click="removeTodo(index)">Remove</button>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {ref} from 'vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    setup(){
+        const newTodo = ref('');
+        const todos = ref([]);
+
+        function addNewTodo(){
+            if(newTodo.value !== ''){
+                todos.value.push({
+                    name: newTodo.value,
+                    id: Date.now(),
+                    done: false,
+                });
+                newTodo.value = '';
+            }
+        }
+
+        function removeTodo(index){
+            todos.value.splice(index, 1);
+        }
+
+        function toggleTodo(todo){
+            todo.done = !todo.done;
+        }
+
+        function markAsDoneAll(){
+            todos.value.forEach(todo => todo.done = true);
+        }
+
+        function removeAll(){
+            todos.value = [];
+        }
+        
+        function removeAllDone(){
+            todos.value.forEach((todo, index) => {
+                if(todo.done){
+                    removeTodo(index);
+                }
+            });
+
+        }
+        
+        return {            
+            newTodo,
+            todos,
+            addNewTodo,
+            removeTodo,
+            toggleTodo,
+            markAsDoneAll,
+            removeAll,
+            removeAllDone,
+        }
+    }
+};
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.done{
+    color: red;
+    text-decoration: line-through;
 }
 </style>
